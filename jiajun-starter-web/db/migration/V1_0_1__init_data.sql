6f96eda24c7833a1e441c5fa43a202a821aac16d
yangjiajun <yangjiajun999@126.com>
2019-08-05 10:53:55 +0800

    初始化数据表

--- jiajun-starter-web/db/migration/V1_0_1__init_data.sql
+++ jiajun-starter-web/db/migration/V1_0_1__init_data.sql
@@ -1,10 +1,6 @@
-INSERT INTO `sys_user` (`id`, `username`, `password`, `orgin`, `realname`, `gender`, `avatar`, `is_freezed`,`dep_id`, `position_id`, `phone`, `leader_id`, `email`, `card_no`, `hiredate`,
- `wrong_time`, `company_id`, `level`, `pwd_modify_time`, `created_user`, `created_time`, `updated_user`, `updated_time`, `is_deleted`)
-VALUES (1, 'admin', '$2a$10$tuv5raVR4jrTMRiVIpPpgOZVw9BJ2vg5/I8ZMPZx3gU3zdmchq1XW', 1, '管理员', 1,
-        '', 0, 0, 0, '13683218469', 1,
-        'yangjj@163.com', '362528198011051051', '2019-01-31 10:30:21', 5, 1, 1, '2019-01-31 10:30:21', 1, '2019-01-25 01:01:01', 1,
-        '2019-01-25 01:01:01', 0);
+-- 系统用户（sys_user）
+INSERT INTO `sys_user` (`id`, `username`, `password`, `salt`, `email`, `mobile`, `status`, `create_user_id`, `create_time`) VALUES ('1', 'admin', '9ec9750e709431dad22365cabc5c625482e574c74adaebba7dd02f1129e4ce1d', 'YzcmCZNvbXocrsz9dm8e', 'yangjj@retechcorp.com', '18112345678', '1', '1', '2011-11-11 11:11:11');
 
 -- 菜单（sys_menu）
-INSERT INTO `sys_menu`(`menu_id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`) VALUES (1, 0, '系统管理', NULL, NULL, 0, 'system', 0);
-INSERT INTO `sys_menu`(`menu_id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`) VALUES (2, 1, '用户管理', NULL, NULL, 0, 'user', 0);
+INSERT INTO `sys_menu`(`id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`) VALUES (1, 0, '系统管理', NULL, NULL, 0, 'system', 0);
+INSERT INTO `sys_menu`(`id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`) VALUES (2, 1, '用户管理', NULL, NULL, 0, 'user', 0);
